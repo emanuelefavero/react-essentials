7a7c2ba97dd0909290bf14777e605d4c010a9c15
emanuelefavero <emafaux89@gmail.com>
2023-12-31 11:09:04 +0100

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import image from './assets/react.svg'
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <main
+      className='w-full h-full flex 
+    justify-center items-center flex-col'
+    >
+      <h1 className='text-violet-200 text-3xl'>Hello</h1>
+      <img src={image} alt='react' />
+    </main>
   )
 }
 
