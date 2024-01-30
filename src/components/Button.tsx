c75660aebd61d206be2ad9bd939b8658a85a8024
emanuelefavero <emafaux89@gmail.com>
2024-01-30 09:31:51 +0100

    add comment in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,8 @@ interface Props {
   onClick: () => void
 }
 
+// TIP: This component takes a function as a prop (onClick) so it can be used as a callback, calling the function on the parent component
+
 export default function Button({ onClick }: Props) {
   return <button onClick={onClick}>Button</button>
 }
