56b6c33fad0447bb7b2658806dd2b1d69773478b
emanuelefavero <emafaux89@gmail.com>
2024-02-18 06:40:23 +0100

    add components/ListWithCustomHook call useTodos

--- src/components/ListWithCustomHook.tsx
+++ src/components/ListWithCustomHook.tsx
@@ -1,3 +1,47 @@
+import { useState } from 'react'
+import useTodos from '../hooks/useTodos'
+import { v4 as uuidv4 } from 'uuid'
+
 export default function ListWithCustomHook() {
-  return <div>ListWithCustomHook</div>
+  const [text, setText] = useState('')
+
+  // TIP: useTodos is a custom hook
+  const { todos, addTodo } = useTodos([
+    { id: uuidv4() as string, text: 'Sleep' },
+    { id: uuidv4() as string, text: 'Eat' },
+    { id: uuidv4() as string, text: 'Work' },
+  ])
+
+  return (
+    <>
+      <h2>ListWithCustomHook</h2>
+
+      {/* Add new todo */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          addTodo(text)
+          setText('')
+        }}
+      >
+        <input
+          className='text-black py-1 px-2'
+          placeholder='Add todo'
+          type='text'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className='bg-fuchsia-500 py-1 px-2' type='submit'>
+          +
+        </button>
+      </form>
+
+      {/* Todos */}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
