97482dbe17bbe4ade8e407ec2b10b014d54a5f31
emanuelefavero <emafaux89@gmail.com>
2024-02-18 09:40:40 +0100

    add components/LazyLoading lazy component

---
+++ src/components/LazyComponent.tsx
@@ -0,0 +1,11 @@
+import './LazyComponent.css'
+
+export default function LazyComponent() {
+  return (
+    <>
+      <h2 className='LazyComponent-animation font-bold font-2xl text-blue-400 bg-blue-900 py-2 px-2'>
+        Lazy Component
+      </h2>
+    </>
+  )
+}
--- src/components/LazyLoading.tsx
+++ src/components/LazyLoading.tsx
@@ -1,7 +1,46 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+const LazyComponent = React.lazy(() => import('./LazyComponent'))
+
 export default function LazyLoading() {
+  const [isVisible, setIsVisible] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+          observer.disconnect()
+        }
+      },
+      {
+        rootMargin: '-100px', // Loads the component when the user is within *px of the element. Adjust this value based on your needs.
+        // TIP: By giving negative values, the component will load only before the user goes beyond the element (which is useful for animations)
+      }
+    )
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+
+    return () => {
+      if (observer) {
+        observer.disconnect()
+      }
+    }
+  }, [])
+
   return (
     <>
-      <h2>LazyLoading</h2>
+      <h2>Lazy Loading</h2>
+      <div ref={ref}>
+        {isVisible && (
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <LazyComponent />
+          </React.Suspense>
+        )}
+      </div>
     </>
   )
 }
