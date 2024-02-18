0244e3c6ff68f3b1185690edcff0cf4500f861dd
emanuelefavero <emafaux89@gmail.com>
2024-02-18 15:31:48 +0100

    add home route with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+])
 
 function App() {
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   )
 }
