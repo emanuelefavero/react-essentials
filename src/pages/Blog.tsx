ebd33d01b8e2399757419955bec09f703c5bfcf9
emanuelefavero <emafaux89@gmail.com>
2024-02-20 05:43:35 +0100

    add search with url search parameter in Blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
-// TIP: In a real application, you would fetch this data from an API
+// TIP: In real world, you would fetch this data from an API
 const posts = [
   {
     id: 1,
@@ -20,15 +21,43 @@ const posts = [
 ]
 
 export default function Blog() {
+  const [searchResults, setSearchResults] = useState(posts)
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search)
+    const searchQuery = queryParams.get('search') // assuming the query param is named 'search'
+
+    if (searchQuery) {
+      const filteredPosts = posts.filter((post) =>
+        post.title.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+      setSearchResults(filteredPosts)
+    } else {
+      setSearchResults(posts)
+    }
+  }, [location.search])
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = event.target.value
+    navigate(`?search=${searchValue}`)
+  }
+
   return (
     <>
       <Link to='/' className='text-cyan-400'>
         Go back to Home
       </Link>
       <h1>Blog</h1>
-
+      <input
+        type='text'
+        placeholder='Search posts...'
+        onChange={handleSearch}
+        className='p-1 text-black'
+      />
       <ul>
-        {posts.map((post) => (
+        {searchResults.map((post) => (
           <li key={post.id}>
             <Link to={`/blog/${post.id}`} className='text-cyan-400'>
               {post.title}
